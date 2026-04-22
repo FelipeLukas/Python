63485907d38e3662259b44801afda673fc36949d
root <root@DESKTOP-DC1KRVO.localdomain>
2026-04-22 18:29:17 -0300

    feat: reiniciando repo com os exercicios

---
+++ aula100426-tiosso-exe02.py
@@ -0,0 +1,8 @@
+a=int(input("Informe um número inteiro: \n"))
+b=int(input("Informe um número inteiro: \n"))
+
+print(f"A soma dos valores é igual à {a+b}")
+print(f"A subtração dos valores é igual à {a-b}")
+print(f"A multiplicação dos valores é igual à {a*b}")
+print(f"A divisão dos valores é igual à {a/b}")
+print(f"O resto da divisão entre os valores é igual à {a%b}")
